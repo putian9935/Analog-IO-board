bd55954259cad8f536ead0bc4cb4da923440e3d6
putian9935 <pl35@u.nus.edu>
2022-05-22 16:31:37 +0800

    Revert "fix bug"
    This reverts commit f780673589a6c6c060840eef54ec00a0decc411f.

--- src/write.cpp
+++ src/write.cpp
@@ -2,8 +2,6 @@
 
 static IMXRT_LPSPI_t* spi_regs = &IMXRT_LPSPI3_S;
 
-static uint64_t dac1_num_mangled, dac2_num_mangled;
-
 /*
  * @brief Fast spi transfer16 for DAC, cf. SPI.h implementation
  *
@@ -12,7 +10,7 @@ static uint64_t dac1_num_mangled, dac2_num_mangled;
  */
 static void transfer_dac24(uint64_t data)
 {
-    while ((spi_regs->FSR & 0xff) > 14)
+    while ((spi_regs->FSR & 0xff) > 15)
         ;
     spi_regs->TDR = data >> 16;
     spi_regs->TDR = (data & 0xFFFF);
@@ -20,6 +18,8 @@ static void transfer_dac24(uint64_t data)
 
 void write(uint8_t ch, uint16_t num)
 {
+    static uint64_t dac1_num_mangled, dac2_num_mangled;
+
     uint64_t new_dac_num = insert_zeros(((((uint32_t)((ch & 3) | DAC_DATA_REG)) << 16) | num));
 
     if (ch < 4)
@@ -32,6 +32,8 @@ void write(uint8_t ch, uint16_t num)
 
 void write_both(uint8_t ch1, uint16_t num1, uint8_t ch2, uint16_t num2)
 {
+    uint64_t dac1_num_mangled, dac2_num_mangled;
+
     dac1_num_mangled = insert_zeros(((((uint32_t)((ch1 & 3) | DAC_DATA_REG)) << 16) | num1)) << 1;
     dac2_num_mangled = insert_zeros(((((uint32_t)((ch2 & 3) | DAC_DATA_REG)) << 16) | num2));
 
