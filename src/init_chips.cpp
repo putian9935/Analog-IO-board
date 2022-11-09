f66a7810770dadec34de9eb8fcf5fcd420fba158
putian9935 <pl35@u.nus.edu>
2022-11-09 22:55:09 +0800

    get DAC right

--- src/init_chips.cpp
+++ src/init_chips.cpp
@@ -7,16 +7,13 @@
 #define FAST_IO IOMUXC_PAD_DSE(4) | IOMUXC_PAD_SPEED(3) | IOMUXC_PAD_SRE
 
 void set_fastio_pin(uint8_t pin_num) {
-	*(portControlRegister(pin_num)) = FAST_IO;
+	// *(portControlRegister(pin_num)) = FAST_IO;
 }
 
 static void prepare_fast_spi_transfer24(){
     pinMode(39, OUTPUT); // new MISO as output
     SPI1.setCS(0);
     set_fastio_pin(0);
-    // old board 
-    // SPI1.setMISO(39); 
-    // set_fastio_pin(39);
     // new board 
     SPI1.setMISO(1);
     set_fastio_pin(1);
@@ -41,7 +38,7 @@ static void init_DAC1()
     digitalWrite(DAC_CLR1, HIGH); 
 
     set_fastio_pin(26);  // MOSI1
-    set_fastio_pin(17);  // SCK1
+    set_fastio_pin(27);  // SCK1
 }
 
 static void init_DAC2()
@@ -63,19 +60,17 @@ void init_chips()
     // This will set a higher IPG root clock
     CCM_CBCDR = (CCM_CBCDR & ~CCM_CBCDR_IPG_PODF_MASK) | CCM_CBCDR_IPG_PODF(1);
     // increase CPU clock speed to 800MHz 
-    // set_arm_clock_cpp(800000000);
-
+    set_arm_clock_cpp(720000000);
+    
     init_DAC1();
     init_DAC2();
-    pinMode(RST_DAC, OUTPUT);
-    digitalWrite(RST_DAC, HIGH);
 
     digitalWrite(LDAC1, LOW);
     digitalWrite(LDAC2, LOW);
 
     delay(20); // wait for chips start-up 
     
-    init_ADC();
+    // init_ADC();
     
     prepare_fast_spi_transfer24();
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,22 @@ void setup()
     while (!Serial)
         ;
     Serial.begin(115200);
+    init_chips();
 }
 
 void loop()
 {
-    auto ic = make_iir_cascade_simulator(
-    {-1./5},
-    {0., -1./3},
-    -250. * 65536 / 6.,
-    [](double const x) {return 4.1e-4 * x * 6 /65536 ;}
-    ); 
+    write(5, 65535);
+    delayMicroseconds(20);
+    write(5, 0);
+    delayMicroseconds(10);
+    write(5, 2);
+    delayMicroseconds(10);
+    write(5, 4);
+    delayMicroseconds(10);
+    write(5, 8);
+    delayMicroseconds(10);
+    write(5, 16);
+    delayMicroseconds(10);
 
-    if(Serial.available())
-    {
-        Serial.read();
-        simulate(ic);
-    }
 }
