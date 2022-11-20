f7b2e518c2fda351860073a93232c7c2d8718673
putian9935 <pl35@u.nus.edu>
2022-11-20 22:43:34 +0800

    add stop

--- terminal/backend.py
+++ terminal/backend.py
@@ -1,8 +1,9 @@
 from ports import setup_arduino_port
 import time 
 import struct
+from ast import literal_eval
 
-ser = setup_arduino_port('COM14')
+ser = setup_arduino_port('COM3')
 
 def sweep(lower, upper):
     ser.write(struct.pack("<BHH", 1, lower, upper))
@@ -12,9 +13,15 @@ def sweep(lower, upper):
         print(ser.readline())
 
 def servo(fi, g, wfm):
-    ser.write(struct.pack("<dd", fi, g))
+    ser.write(struct.pack("<Bdd", 2, eval(fi.strip('"')), g))
     ser.write(wfm)
+    while not ser.in_waiting:
+        time.sleep(.5)
+    while ser.in_waiting:
+        print(ser.readline())
 
-
+def stop(args):
+    ser.write(b'\x00')
+    
 if __name__ == '__main__':
     print(sweep(2, 1500))
--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,6 +1,6 @@
 import argparse
 
-from backend import sweep, servo
+from backend import sweep, servo, stop
 from csv_reader import get_wfm 
 
 def help_action(args):
@@ -15,17 +15,12 @@ def servo_action(args):
         open(args.fname)
     except FileNotFoundError as e:
         print(e)
-    servo(args.f_I, args.g, get_wfm(args.fname))
+    servo(args.f_I, args.G, get_wfm(args.fname))
+    
     
 
 parser = argparse.ArgumentParser('', description="Intesnity servo terminal", add_help=False, epilog="Use 'help command name' to see how to use each command.", exit_on_error=False)
 subparsers = parser.add_subparsers()
-
-
-help_parser = subparsers.add_parser("help", 
-description='Display help for commands', add_help=False, exit_on_error=False)
-help_parser.add_argument('cmd', type=str, choices=['sweep', 'servo', 'help'], help='command name')
-help_parser.set_defaults(func=help_action)
 
 sweep_parser = subparsers.add_parser("sweep", description="Start sweep", add_help=False, exit_on_error=False)
 sweep_parser.add_argument('--lower', type=int, required=False, default=0, help='Lower limit of DAC number. Default is 0. ')
@@ -38,6 +33,14 @@ servo_parser.add_argument('f_I', metavar='f_I', type=str, help='I corner in the 
 servo_parser.add_argument('G', metavar='G', type=float, help='Overall gain')
 servo_parser.add_argument('fname', metavar='fname', type=str, help='Reference waveform file')
 servo_parser.set_defaults(func=servo_action)
+
+stop_parser = subparsers.add_parser("stop", description="Stop current command", add_help=False, exit_on_error=False)
+stop_parser.set_defaults(func=stop)
+
+help_parser = subparsers.add_parser("help", 
+description='Display help for commands', add_help=False, exit_on_error=False)
+help_parser.add_argument('cmd', type=str, choices=subparsers.choices.keys(), help='command name')
+help_parser.set_defaults(func=help_action)
 
 parser.print_help()
 
