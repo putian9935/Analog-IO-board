cdea9f130a4c3777bf7a7740c65f29f535a33aa4
putian9935 <pl35@u.nus.edu>
2023-03-27 17:42:00 +0000

    Update docs

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -4,9 +4,11 @@ var searchData=
   ['advanced_20uses_20of_20analog_20read_1',['Advanced uses of analog read',['../read_howto.html',1,'']]],
   ['analog_20io_20board_20library_20support_2',['Analog IO board library support',['../index.html',1,'']]],
   ['analog_5fio_2eh_3',['analog_io.h',['../analog__io_8h.html',1,'']]],
-  ['analogio_5fread_4',['analogio_read',['../analog__io_8h.html#a4258fe084d100d6babeab3bc0d5691d1',1,'analog_io.cpp']]],
-  ['analogio_5fread_5fboth_5',['analogio_read_both',['../analog__io_8h.html#a0aa1383862b9d247135ccedcab696682',1,'analog_io.cpp']]],
-  ['analogio_5fwrite_6',['analogio_write',['../analog__io_8h.html#a5c44562f0b91e66b94221d0ff78e1552',1,'analog_io.h']]],
-  ['analogio_5fwrite_5fboth_7',['analogio_write_both',['../analog__io_8h.html#ae0326502f43aa9a6f35bd75d5383f022',1,'analog_io.h']]],
-  ['anonymous_5fnamespace_7bread_2ecpp_7d_8',['anonymous_namespace{read.cpp}',['../namespaceanonymous__namespace_02read_8cpp_03.html',1,'']]]
+  ['analog_5fread_2eino_4',['analog_read.ino',['../analog__read_8ino.html',1,'']]],
+  ['analog_5fwrite_2eino_5',['analog_write.ino',['../analog__write_8ino.html',1,'']]],
+  ['analogio_5fread_6',['analogio_read',['../analog__io_8h.html#a4258fe084d100d6babeab3bc0d5691d1',1,'analog_io.cpp']]],
+  ['analogio_5fread_5fboth_7',['analogio_read_both',['../analog__io_8h.html#a0aa1383862b9d247135ccedcab696682',1,'analog_io.cpp']]],
+  ['analogio_5fwrite_8',['analogio_write',['../analog__io_8h.html#a5c44562f0b91e66b94221d0ff78e1552',1,'analog_io.h']]],
+  ['analogio_5fwrite_5fboth_9',['analogio_write_both',['../analog__io_8h.html#ae0326502f43aa9a6f35bd75d5383f022',1,'analog_io.h']]],
+  ['anonymous_5fnamespace_7bread_2ecpp_7d_10',['anonymous_namespace{read.cpp}',['../namespaceanonymous__namespace_02read_8cpp_03.html',1,'']]]
 ];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['analog_5fio_2eh_0',['analog_io.h',['../analog__io_8h.html',1,'']]]
+  ['analog_5fio_2eh_0',['analog_io.h',['../analog__io_8h.html',1,'']]],
+  ['analog_5fread_2eino_1',['analog_read.ino',['../analog__read_8ino.html',1,'']]],
+  ['analog_5fwrite_2eino_2',['analog_write.ino',['../analog__write_8ino.html',1,'']]]
 ];
