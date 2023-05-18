69c3ee127ca0641d7730c4c22a6ba854ace05bb0
putian9935 <pl35@u.nus.edu>
2023-05-18 11:12:16 +0800

    add reference hsp

--- analog_io/src/servo/controller.hpp
+++ analog_io/src/servo/controller.hpp
@@ -9,18 +9,21 @@
 #define CONTROLLER_HPP
 
 #include <Arduino.h>
-
 #include <cmath>
 #include <cstring>
 #include <vector>
-
 #include "is_int.hpp"
 #include "reference.hpp"
 #include "serial_reader.hpp"
 
 struct Controller;
 
-enum Command : uint8_t { SWEEP = 1, SERVO, CHANNEL, HSP, SHOW, REF };
+enum Command : uint8_t { SWEEP = 1,
+                         SERVO,
+                         CHANNEL,
+                         HSP,
+                         SHOW,
+                         REF };
 
 typedef double (*read_func_t)();
 typedef void (*write_func_t)(double);
@@ -28,26 +31,27 @@ typedef void (*write_func_t)(double);
 // controller base class
 struct Controller {
     bool on;
-    uint16_t hold_output;
+    // uint16_t hold_output;
     read_func_t reader;
     write_func_t writer;
     ReferencePath* reference;
+    ReferencePath* reference_hsp;
     double lower, upper;
 
-    Controller(read_func_t reader, write_func_t writer, ReferencePath* ref,
+    Controller(read_func_t reader, write_func_t writer, ReferencePath* ref, ReferencePath* ref_hsp,
                double const lower, double const upper)
         : on(false),
-          hold_output(0),
           reader(reader),
           writer(writer),
           reference(ref),
+          reference_hsp(ref_hsp),
           lower(lower),
           upper(upper) {}
-    virtual void update() = 0;
+    virtual void update()                        = 0;
     virtual void read_from_serial(uint8_t const) = 0;
     virtual ~Controller(){};
 };
 
-typedef Controller * const pController; 
+typedef Controller* const pController;
 
 #endif  // CONTROLLER_HPP
