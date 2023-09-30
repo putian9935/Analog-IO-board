b1c386c4935eb9883d7a8088823152776b1aeb3f
putian9935 <pl35@u.nus.edu>
2023-09-30 02:48:19 +0000

    Update docs

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['read_5fain0_0',['read_ain0',['../read_8cpp.html#a195fc011464385e201399a9dad173e9c',1,'read_ain0():&#160;read.cpp'],['../read_8hpp.html#a195fc011464385e201399a9dad173e9c',1,'read_ain0():&#160;read.cpp']]],
-  ['read_5fain1_1',['read_ain1',['../read_8cpp.html#aa62b29b328a23a09a57f5735ba41ac34',1,'read_ain1():&#160;read.cpp'],['../read_8hpp.html#aa62b29b328a23a09a57f5735ba41ac34',1,'read_ain1():&#160;read.cpp']]],
-  ['read_5fbin0_2',['read_bin0',['../read_8cpp.html#a51cfc72d171f4dae253654e6385d3276',1,'read_bin0():&#160;read.cpp'],['../read_8hpp.html#a51cfc72d171f4dae253654e6385d3276',1,'read_bin0():&#160;read.cpp']]],
-  ['read_5fbin1_3',['read_bin1',['../read_8cpp.html#a00058a893daa6e08ff6a83f580906ba3',1,'read_bin1():&#160;read.cpp'],['../read_8hpp.html#a00058a893daa6e08ff6a83f580906ba3',1,'read_bin1():&#160;read.cpp']]]
+  ['read_5fbin0_1',['read_bin0',['../read_8cpp.html#a51cfc72d171f4dae253654e6385d3276',1,'read_bin0():&#160;read.cpp'],['../read_8hpp.html#a51cfc72d171f4dae253654e6385d3276',1,'read_bin0():&#160;read.cpp']]]
 ];
